de88f1e72615365bb42d8523699b64e407940138
Baris Terzioglu, Umut <Umut.Baris-Terzioglu@adesso-insurance-solutions.de>
2026-04-18 20:43:25 +0200

    sync

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import HeroSection from "@/components/HeroSection";
 import AboutSection from "@/components/AboutSection";
 import CategoriesSection from "@/components/CategoriesSection";
@@ -7,6 +8,10 @@ import SupportSection from "@/components/SupportSection";
 import FooterSection from "@/components/FooterSection";
 
 const Index = () => {
+  useEffect(() => {
+    document.dispatchEvent(new Event("render-complete"));
+  }, []);
+
   return (
     <div className="min-h-screen">
       <HeroSection />
