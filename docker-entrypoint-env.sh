9169c01fc096da74a7eaadf9f0e702e2677dd095
Baris Terzioglu, Umut <Umut.Baris-Terzioglu@adesso-insurance-solutions.de>
2026-04-27 19:08:37 +0200

    founder

--- docker-entrypoint-env.sh
+++ docker-entrypoint-env.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -eu
 
+escaped_rag_api_secret=$(
+  printf '%s' "${RAG_API_SECRET:-}" | sed -e 's/[|&\\]/\\&/g' -e 's/[$]/$$/g'
+)
+
+sed -i "s|__RAG_API_SECRET__|$escaped_rag_api_secret|g" /etc/nginx/conf.d/default.conf
+
 cat <<EOF >/usr/share/nginx/html/env-config.js
 window.__APP_CONFIG__ = {
   VITE_SUPABASE_URL: "${VITE_SUPABASE_URL:-}",
